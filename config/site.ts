5ba70a5a1d4c0904c2ab0545ac9967ae13a39231
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-10-25 21:29:33 +0200

    chore: cleanup, updating details and fixing redirects

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Lennard Zündorf",
+	description: "Personal Website and Portfolio",
 	navItems: [
 		{
 			label: "Home",
@@ -32,10 +32,8 @@ export const siteConfig = {
 		},
 	],
 	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+		github: "/github",
+		linkedin: "/linkd",
+		mail: "/mail"
 	},
 };
