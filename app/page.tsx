e3c6222f052812cf3542a5dee8f5bc9adad2d70b
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-10-26 15:05:06 +0200

    feat: update navbar, new color switch toggle and extracted footer component

--- app/page.tsx
+++ app/page.tsx
@@ -21,14 +21,6 @@ export default function Home() {
 					Beautiful, fast and modern React UI library.
 				</h2>
 			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
 		</section>
 	);
 }
