1afcf29081c5f0c80334ac93985f85a356bc07b4
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-11-03 16:28:22 +0100

    feat: adding footer, legal page

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,62 @@
+"use client";
+
 import {Link} from "@nextui-org/link";
+import { siteConfig } from "@/config/site";
+import React from "react";
+import NextLink from "next/link";
+import {Chip} from "@nextui-org/react";
+import {HiAtSymbol} from 'react-icons/hi2';
+import {AiFillLinkedin, AiOutlineGithub} from 'react-icons/ai';
+
 export const Footer = () => {
-    return (
-    <footer className="w-full flex items-center justify-center py-3">
-        <Link
-            isExternal
-            className="flex items-center gap-1 text-current"
-            href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-            title="nextui.org homepage"
-        >
-            <span className="text-default-600">Powered by</span>
-            <p className="text-primary">NextUI</p>
-        </Link>
-    </footer>
-    );
-};
+    {
+        return (
+            <footer>
+                <div className="mt-16 flex flex-col items-center">
+                    <div className="mb-3 flex space-x-4">
+                        <div className="flex gap-4">
+                            <Link href={siteConfig.links.github}>
+                                <Chip
+                                    startContent={<HiAtSymbol size={20}/>}
+                                    variant="shadow"
+                                    color="default"
+                                >
+                                    Mail
+                                </Chip>
+                            </Link>
+
+                            <Link href={siteConfig.links.github}>
+                                <Chip
+                                    startContent={<AiFillLinkedin size={20} />}
+                                    variant="shadow"
+                                    color="default"
+                                >
+                                    LinkedIn
+                                </Chip>
+                            </Link>
+
+                            <Link href={siteConfig.links.github}>
+                                <Chip
+                                    startContent={<AiOutlineGithub size={20}/>}
+                                    variant="shadow"
+                                    color="default"
+                                >
+                                    Github
+                                </Chip>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="mb-2 flex space-x-2 text-sm ">
+                        <div>{siteConfig.name}</div>
+                        <div>{` • `}</div>
+                        <div>{`© ${new Date().getFullYear()}`}</div>
+                    </div>
+                    <div className="mb-8 text-sm">
+                        <Link href="/legal" color="foreground" underline="hover">Legal - Datenschutzbestimmungen</Link>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
+
