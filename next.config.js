d367ce28de8c7c680ec953d31723ad316d2ce5a8
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-08 23:46:49 +0100

    chore: updating config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/linkd',
+                destination: 'https://www.linkedin.com/in/lennard-zuendorf/',
+                permanent: true,
+            },
+            {
+                source: '/github',
+                destination: 'https://www.github.com/lennardzuendorf',
+                permanent: true,
+            },
+            {
+                source: '/linkedin',
+                destination: '/linkd',
+                permanent: true,
+            },
+            {
+                source: '/mail',
+                destination: 'mailto:lennard@zuendorf.me',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig
