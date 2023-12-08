ab2a0af7c6b34ef51984ab8a9e44572a7a6b59ac
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-08 16:57:10 +0100

    chore: deleting old files

--- types/index.ts
+++
@@ -1,9 +0,0 @@
-import {SVGProps} from "react";
-
-export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size: number,
-  top: string,
-	bottom: string,
-	gradientStart: string,
-	gradientStop: string;
-};
