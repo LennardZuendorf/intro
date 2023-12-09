d72b9b538438a916a699103b89d7b5df72aa64e6
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-09 09:40:34 +0100

    feat: new color/theme toggles

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import * as React from "react"
+import { IoColorFilterSharp,IoColorFilterOutline  } from 'react-icons/io5';
 import { useTheme } from "next-themes"
-import {IoSunny, IoMoon } from 'react-icons/io5';
 
 import { Button } from "@/components/ui/button"
 import {
@@ -12,27 +12,24 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function ColorToggle() {
+export function ThemeToggle() {
     const { setTheme } = useTheme()
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <IoSunny  className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <IoMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <IoColorFilterSharp className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <IoColorFilterOutline className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
                 <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
+                    Colorful
                 </DropdownMenuItem>
                 <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
+                    Monochrome
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
