6e24571a20b5cf7c6b57dd8bf584f6dd1354d719
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-09 09:42:41 +0100

    chore: setting up page structure and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+import {GridTitle} from '@/components/grid-title'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-
-    </main>
+    <section className="flex flex-col items-center gap-2 sm:gap-4 md:gap-8 lg:gap-16 py-8 md:py-16 lg:py-24">
+      <div id="title-hero" className="block text-center justify-center">
+        <h1 className="text-4xl font-extrabold lg:text-5xl">
+          Welcome to My Portfolio Site!
+        </h1>
+        <p className="text-xl text-muted-foreground">
+          Here you can find all the information about me and my projects.
+        </p>
+      </div>
+      <div>
+        <GridTitle />
+      </div>
+    </section>
   )
 }
