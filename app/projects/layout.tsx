bf1152edf300d333b70d6482b2416b35ee128802
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-11 16:58:49 +0100

    feat: implemented new nav for projects

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,8 +1,10 @@
 import { cn } from "@/lib/utils"
+import {ProjectsNav} from "@/components/pages/nav-projects";
 
 export default function ProjectLayout({children,}: { children: React.ReactNode }) {
     return (
         <section className={cn("flex flex-col")}>
+            <ProjectsNav />
             <div className="inline-block text-center justify-center">
                 {children}
             </div>
