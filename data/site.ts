2ddf2a7d200ad9b54622283e48d6c5fc47712df1
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-11 15:40:38 +0100

    chore: moving siteConfig

---
+++ data/site.ts
@@ -0,0 +1,46 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    name: "zuendorf.me",
+    title: "Lennard Zündorf",
+    description: "Personal Website and Portfolio",
+    navItems: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "Projects",
+            href: "/projects",
+        },
+        {
+            label: "About",
+            href: "/about",
+        },
+    ],
+    links: {
+        github: "/github",
+        linkedin: "/linkd",
+        mail: "/mail"
+    },
+};
+
+export type SiteMetadata = typeof siteMetadata;
+
+export const siteMetadata = {
+    title: 'ignitr',
+    author: 'Lennard',
+    description: 'Lennard Zuendorfs project and showcase portfolio website',
+    headerTitle: 'ignitr',
+    language: 'en-us',
+    theme: 'system',
+    siteUrl: 'https://ignitr.tech',
+    siteRepo: 'https://github.com/lennardzuendorf/ignitus',
+    siteLogo: '/static/images/logo.png',
+    image: '/static/images/avatar.png',
+    socialBanner: '/static/images/social-preview.png',
+    email: 'lennard@zuendorf.me',
+    github: 'https://zuendorf.me/github',
+    linkedin: 'https://zuendorf.me/linkd',
+    locale: 'en-de',
+}
