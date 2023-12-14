57a1a10ccb5639b4f2c7f0a7f454a19044c99082
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-14 16:29:37 +0100

    feat: first project page, fixing project navigation

--- components/pages/nav-projects.tsx
+++ components/pages/nav-projects.tsx
@@ -13,14 +13,21 @@ import {
     NavigationMenuList,
     navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import {compareDesc, parseISO} from "date-fns";
 
 export const ProjectsNav = () => {
     const pathname = usePathname();
+    const focusProjects = allProjects
+        .filter((project) => project.focus)
+        .sort((a, b) => {
+            return compareDesc(parseISO(String(a.date)), parseISO(String(b.date)))
+        })
+
     return (
-        <div className={cn("flex flex-col items-center pt-4 pb-4 gap-1")}>
-            <NavigationMenu className="rounded-md border">
+        <div className={cn("flex flex-col items-center gap-1")}>
+            <NavigationMenu className="rounded-md">
                 <NavigationMenuList>
-                    {allProjects.map((item) => (
+                    {focusProjects.map((item) => (
                         <NavigationMenuItem key={item.url}>
                             <Link href={item.url} legacyBehavior passHref>
                                 <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), pathname === item.url ? 'font-black' : item.focus ? 'font-semi-bold': 'font-medium')} active={pathname === item.url}>
