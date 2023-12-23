d4c7d2c0b9e114efd1ff1ccb0739f4d3650d1d11
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-12-23 11:42:05 +0100

    feat: adding prettier, husky and lint-staged for code quality - first run

--- next.config.js
+++ next.config.js
@@ -3,35 +3,31 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/linkd',
-                destination: 'https://www.linkedin.com/in/lennard-zuendorf/',
-                permanent: true,
-            },
-            {
-                source: '/github',
-                destination: 'https://www.github.com/lennardzuendorf',
-                permanent: true,
-            },
-            {
-                source: '/linkedin',
-                destination: '/linkd',
-                permanent: true,
-            },
-            {
-                source: '/mail',
-                destination: 'mailto:lennard@zuendorf.me',
-                permanent: true,
-            },
-            {
-                source: '/projects/thesis/preview',
-                destination: 'https://huggingface.co/spaces/LennardZuendorf/thesis-webapp-docker',
-                permanent: true,
-            }
-        ];
-    },
+  async redirects() {
+    return [
+      {
+        source: "/linkd",
+        destination: "https://www.linkedin.com/in/lennard-zuendorf/",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://www.github.com/lennardzuendorf",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "/linkd",
+        permanent: true,
+      },
+      {
+        source: "/projects/thesis/preview",
+        destination:
+          "https://huggingface.co/spaces/LennardZuendorf/thesis-webapp-docker",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
