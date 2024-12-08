fadec9ce0bf211d5eba9da2721270c25ecf39d74
Lennard Zündorf <l.p.zuendorf@gmail.com>
2024-12-08 13:28:37 +0100

    chore: updating ci, config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 import { withPayload } from '@payloadcms/next/withPayload';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/linkedin",
+        destination: "/linkd",
+        permanent: true,
+      },
+      {
+        source: "/linkd",
+        destination: "https://www.linkedin.com/in/lennard-zuendorf/",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://www.github.com/lennardzuendorf",
+        permanent: true,
+      },
+      {
+        source: "/cv",
+        destination: "https://www.zuendorf.me/docs/cv.pdf",
+        permanent: true,
+      },
+    ];
+  }
+};
 
 export default withPayload(nextConfig);
