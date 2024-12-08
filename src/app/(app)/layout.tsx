ed0df01471b4dc48408d20bd446411905e1860c8
Lennard Zündorf <l.p.zuendorf@gmail.com>
2024-12-08 14:08:41 +0100

    chore: adding dark mode, analytics, speedInsights

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 import '@/app/main.css';
+import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <html lang='en' suppressHydrationWarning>
-        <body className={cn('relative min-h-screen bg-background font-sans antialiased')}>
-          <div
-            className={cn(
-              'container mx-auto grow place-content-center w-full md:w-9/12 2xl:min-h-[95vh] xl:min-h-[75vh]'
-            )}
-          >
-            {children}
-          </div>
-        </body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <body className={cn('relative min-h-screen bg-background font-sans antialiased')}>
+            <div
+              className={cn(
+                'container mx-auto grow place-content-center w-full md:w-9/12 2xl:min-h-[95vh] xl:min-h-[75vh]'
+              )}
+            >
+              {children}
+            </div>
+          </body>
+        </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </html>
     </>
   );
