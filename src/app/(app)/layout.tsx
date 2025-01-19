72c07613f03edca28bd750318549444a09eb84c4
Lennard Zündorf <l.p.zuendorf@gmail.com>
2025-01-19 01:50:43 +0100

    fix: fixing broken things

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,34 +4,14 @@ import { Footer } from '@/components/footer';
 import { Providers } from '@/components/providers';
 import { siteMetadata } from '@/data/site';
 import { cn } from '@/lib/utils';
-import { IconHome, IconMessage, IconUser } from '@tabler/icons-react';
 import Script from 'next/script';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const navItems = [
-    {
-      name: 'Home',
-      link: '/',
-      icon: <IconHome className='h-4 w-4 text-neutral-500 dark:text-white' />
-    },
-    {
-      name: 'About',
-      link: '/about',
-      icon: <IconUser className='h-4 w-4 text-neutral-500 dark:text-white' />
-    },
-    {
-      name: 'Contact',
-      link: '/contact',
-      icon: <IconMessage className='h-4 w-4 text-neutral-500 dark:text-white' />
-    }
-  ];
-
   return (
     <>
       <html lang='en' suppressHydrationWarning>
         <head>
           <title>{siteMetadata.title}</title>
-          <link rel='icon' href='/favicon.ico' sizes='any' />
           <Script
             src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_SRC}
             data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
