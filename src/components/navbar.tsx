a0e5df6ea136fd0a3141ff8288fcce4b5ff0e0b6
Lennard Zündorf <l.p.zuendorf@gmail.com>
2025-01-24 21:31:13 +0100

    fix: fixing broken things

---
+++ src/components/navbar.tsx
@@ -0,0 +1,63 @@
+'use client';
+import React, { useState } from 'react';
+import { motion, AnimatePresence, useScroll, useMotionValueEvent } from 'framer-motion';
+import { cn } from '@/lib/utils';
+import { HomeIcon, MessageCircleIcon, UserIcon } from 'lucide-react';
+import { MainNav } from '@/components/custom/nav-menu';
+
+export const Nav = ({ className }: { className?: string }) => {
+  const navItems = [
+    {
+      name: 'Home',
+      link: '/',
+      icon: <HomeIcon className='h-4 w-4 text-neutral-500 dark:text-white' />
+    },
+    {
+      name: 'About',
+      link: '/about',
+      icon: <UserIcon className='h-4 w-4 text-neutral-500 dark:text-white' />
+    },
+    {
+      name: 'Contact',
+      link: '/contact',
+      icon: <MessageCircleIcon className='h-4 w-4 text-neutral-500 dark:text-white' />
+    }
+  ];
+
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, 'change', (current) => {
+    const direction = current! - scrollYProgress.getPrevious()!;
+    if (scrollYProgress.get() < 0.05) {
+      setVisible(false);
+    } else {
+      setVisible(direction < 0);
+    }
+  });
+
+  return (
+    <AnimatePresence mode='wait'>
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0
+        }}
+        transition={{
+          duration: 0.2
+        }}
+        className={cn(
+          'flex max-w-fit fixed top-10 inset-x-0 mx-auto z-[5000] pr-2 pl-2 py-2 items-center justify-center space-x-4',
+          className
+        )}
+      >
+        <MainNav navItems={navItems} />
+      </motion.div>
+    </AnimatePresence>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
 import type { Config } from 'tailwindcss';
 import typographyPlugin from '@tailwindcss/typography';
+import animatePlugin from 'tailwindcss-animate';
 
 const config: Config = {
-  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: ['class'],
+  content: [
+    './src/**/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -14,6 +20,58 @@ const config: Config = {
         mono: 'var(--font-mono)',
         title: 'var(--font-spartan)'
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))'
+        },
+        'color-1': 'hsl(var(--color-1))',
+        'color-2': 'hsl(var(--color-2))',
+        'color-3': 'hsl(var(--color-3))',
+        'color-4': 'hsl(var(--color-4))',
+        'color-5': 'hsl(var(--color-5))'
+      },
       keyframes: {
         'accordion-down': {
           from: { height: '0' },
@@ -22,14 +80,24 @@ const config: Config = {
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' }
+        },
+        rainbow: {
+          '0%': {
+            'background-position': '0%'
+          },
+          '100%': {
+            'background-position': '200%'
+          }
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        rainbow: 'rainbow var(--speed, 2s) infinite linear'
       }
     }
   },
-  plugins: [typographyPlugin]
+  plugins: [typographyPlugin, animatePlugin]
 };
+
 export default config;
