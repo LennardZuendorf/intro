3272bf3af70f76c8e5c8e400150c63752cbe4edd
Lennard Zündorf <l.p.zuendorf@gmail.com>
2025-03-31 00:49:54 +0200

    chore: updating config for ci

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250208_135046 from './20250208_135046';
+import * as migration_20250330_223430 from './20250330_223430';
 
 export const migrations = [
   {
     up: migration_20250208_135046.up,
     down: migration_20250208_135046.down,
     name: '20250208_135046'
+  },
+  {
+    up: migration_20250330_223430.up,
+    down: migration_20250330_223430.down,
+    name: '20250330_223430'
   }
 ];
