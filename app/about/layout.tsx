a525e7259b9cc4dad87c80218bb312b5a131fe20
Lennard Zuendorf <l.p.zuendorf@gmail.com>
2025-12-08 18:15:16 +0100

    refactor: remove About page components and related files for a simplified structure

--- app/about/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from '@/lib/utils';
-
-export default function AboutLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className={cn('flex flex-col h-max')}>
-      <div className='inline-block text-center justify-center'>{children}</div>
-    </section>
-  );
-}
